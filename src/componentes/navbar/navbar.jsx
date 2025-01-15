a6c283989ae79b76254b72f624e3d97a8bf2ffa6
Juan Rubio <juanpablo.rubio990221@gmail.com>
2025-01-15 00:31:00 -0500

    se crea carpetas de assets y se agrega imagen de hamburguesa menu

--- src/componentes/navbar/navbar.jsx
+++ src/componentes/navbar/navbar.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";  // useState: Es como una libreta donde puedes guardar y cambiar información en tu componente.
 //importamos el usestate porque vamos a definir si la hamburguesa esta abierta o cerrada (para hacerlo mas responsivo)
-import {getImageUrl} from "../../utils";
-import Styles from "./Navbar.module.css"; 
+import { getImageUrl } from "../../utils";
+import Styles from "./Navbar.module.css";
 
 export const Navbar = () => {
     return (
         <nav className={Styles.navbar}>
             <a className={Styles.titulo} href='/'>Portafolio</a>
             <div className={Styles.menu}>
+                <img className={Styles.menuBtn} src={getImageUrl("nav/menuIcon.png")}>
+                </img>
                 <ul className={Styles.menuItems}>
                     <li>
                         <a href='#acerca'>Acerca de</a>
